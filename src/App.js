dbd9426319a09639beb067ced7d8f75edc618292
hiwoogi <rmcprpwjd001@gmail.com>
2023-10-27 17:45:35 +0900

    finish part 6

--- src/App.js
+++ src/App.js
@@ -1,69 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import PropTypes from 'prop-types';
-function Food({name, picture, rating}) {
-  return(
-  <div>
-     <h3>I love {name}</h3>
-     <h4>{rating}/5.0</h4>
-     <img src={picture} alt={name}/>
-  </div> 
-  );
+import axios from 'axios';
 
-}
+class App extends React.Component {
 
-const foodILike = [
-  {
-    id: 1,
-    name: 'kimchi',
-    image :'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-    rating : 5
-  },
-  {
-    id:  2,
-    name: 'Samgyeopsal',
-    image :'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
-    rating:4.9
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image:
-      'http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb',
-    rating: 5,
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
-    rating: 5,
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
-    rating: 5,
-  },
-]
+  state = {
+    isLoading : true,
+    movies : [],
+  }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+  getMovies = async() => {
+    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+  }
 
-function renderFood(dish){
-  return (<Food name = {dish.name} picture={dish.image}/>)
-}
+  componentDidMount(){
+    this.getMovies()
+  }
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => <Food key = {dish.id} name = {dish.name} picture={dish.image} rating={dish.rating}/>)}
-      <Food name="kimchi"/>
-    </div>
-   
-  );
+  componentDidUpdate(){
+    console.log('I"m just updated')
+  }
+
+  componentWillUnmount(){
+      
+  }
+
+  render() {
+    const {isLoading} = this.state
+
+    console.log('render')
+    return (
+      <div>
+        {isLoading ? 'Loading': 'we are ready'}
+
+      </div>
+    );
+  }
 }
 
 export default App;
