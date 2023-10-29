6eb05ea6b82046cfe0fdc82bfbc46a3f0e81978e
hiwoogi <rmcprpwjd001@gmail.com>
2023-10-30 00:34:09 +0900

    part7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
 
@@ -10,30 +11,54 @@ class App extends React.Component {
   }
 
   getMovies = async() => {
-    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    const {data : {
+      data : {
+        movies
+      }
+    }} = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading : false})
+    
   }
 
   componentDidMount(){
     this.getMovies()
   }
 
-  componentDidUpdate(){
-    console.log('I"m just updated')
-  }
+  // componentDidUpdate(){
+  //   console.log('I"m just updated')
+  // }
 
-  componentWillUnmount(){
+  // componentWillUnmount(){
       
-  }
+  // }
 
   render() {
-    const {isLoading} = this.state
+    const {isLoading, movies} = this.state
 
-    console.log('render')
+    
     return (
-      <div>
-        {isLoading ? 'Loading': 'we are ready'}
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ): 
+        <div className="movies">
+          {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+        </div>
+        }
 
-      </div>
+      </section>
     );
   }
 }
